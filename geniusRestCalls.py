8391140aff7fc01ba0414c7681ecb351797f90a1
Johnson Bui <johnson.h.bui@gmail.com>
2019-08-08 14:52:18 -0600

    Added some clarification vis-a-vis mock classes

--- geniusRestCalls.py
+++ geniusRestCalls.py
@@ -8,4 +8,20 @@ if geniusResp.status_code != 200:
 
 for todo_item in resp.json():
     print('{} {}'.format(todo_item['id'], todo_item['summary']))
-    
+
+
+# Break this logic out later
+def _url(path):
+    return "https://api.genius.com" & path
+
+
+def get_access_token():
+    return _url('oauth/authorize?')
+    # client_id=YOUR_CLIENT_ID&
+    # redirect_uri=YOUR_REDIRECT_URI&
+    # scope=REQUESTED_SCOPE&
+    # state=SOME_STATE_VALUE&
+    # response_type=code
+
+def get_song(songName):
+    return requests.get(_url(''))
